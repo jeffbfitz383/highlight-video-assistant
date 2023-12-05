175a2032814079bc6c368d013c70179b1910757c
Jeffrey <jefbfitzp33@outlook.com>
2023-12-05 12:50:34 -0700

    db.json fluffed out

--- src/Playcard.js
+++ src/Playcard.js
@@ -2,13 +2,16 @@ import React from "react";
 import Playlist from "./Playlist";
 
 function Playcard({play}){
-    const {id,player} = play
+    const {id,level,game,quarter,start,stop,player,playtype,quality,assist} = play
 
     return(
         
         <div>
-        <p>{id}</p>
-        <p>{player}</p>
+        
+        <p>{`Play#: ${id}, Level: ${level}, Game: ${game}, Quarter:
+         ${quarter}, Start time: ${start}, Stop time: ${stop}, 
+         Player#:  ${player}, Play Type: ${playtype}, Quality: ${quality},
+          Assist: ${assist}`}</p>
         </div>
     )
 }
