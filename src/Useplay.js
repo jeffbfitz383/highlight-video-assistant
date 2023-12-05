9cd802c1e8ffecd9c96f8e5b3e8e2ba9d4a0fa57
Jeffrey <jefbfitzp33@outlook.com>
2023-12-04 23:02:02 -0700

    db.json data console.logs

--- src/Useplay.js
+++ src/Useplay.js
@@ -1,6 +1,20 @@
 import {useState, useEffect} from 'react'
 
 function Useplay(){
+
+
+
+    const [plays,setPlays] = useState([]) 
+    
+    useEffect(()=>{
+        fetch('http://localhost:3001/plays')
+        .then(r=>r.json())
+        .then(data=>setPlays(data))
+        
+      },[])
+      console.log(plays)
+
+
     return(
         <div>useplay function renders</div>
     )
