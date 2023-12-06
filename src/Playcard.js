fbf1355625854e097b4301f980536da008c2a7a3
Jeffrey <jefbfitzp33@outlook.com>
2023-12-06 12:25:28 -0700

    used button toggle

--- src/Playcard.js
+++ src/Playcard.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import Playlist from "./Playlist";
 
 function Playcard({play}){
+    const [used,setUsed] = useState(false)
     const {id,level,game,quarter,start,stop,player,playtype,quality,assist} = play
 
     return(
@@ -11,7 +12,11 @@ function Playcard({play}){
         <p>{`Play#: ${id}, Level: ${level}, Game: ${game}, Quarter:
          ${quarter}, Start time: ${start}, Stop time: ${stop}, 
          Player#:  ${player}, Play Type: ${playtype}, Quality: ${quality},
-          Assist: ${assist}`}</p>
+          Assist: ${assist}`}  {used ? (
+            <button onClick={()=>setUsed(false)}>Used</button>
+          ) : (
+            <button  onClick={()=>setUsed(true)}>Unused</button>
+          )}</p>
         </div>
     )
 }
