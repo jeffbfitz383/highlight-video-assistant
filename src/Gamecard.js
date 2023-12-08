bfe3d39e8650549b1da54211c9bbfe9a64bcd28c
Jeffrey <jefbfitzp33@outlook.com>
2023-12-08 12:00:52 -0700

    games post

--- src/Gamecard.js
+++ src/Gamecard.js
@@ -2,11 +2,15 @@ import {useState, useEffect} from 'react'
 import './styles.css';
 
 
-function Gamecard(game){
+function Gamecard({game}){
     console.log(game)
-    return 
-    (
-        <div>game card</div>
+    return (
+        <div>
+        <p>{`Game ID:  ${game.id}`}</p>
+        <p>{`School Name: ${game.school}`}</p>
+        <p>{`Times Used: ${game.timesused}`}</p>
+        <p></p>
+        </div>
     )
 }
 export default Gamecard
